98380fd929487248fd7472e600ed054da847a38e
wicked1234567 <ly1ngz3n@gmail.com>
2024-09-30 18:28:54 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,21 +1,16 @@
 // Objek untuk menyimpan semua pengaturan profil
 const profileSettings = {
   favicon: "media/vynaa.jpg", // Path ke favicon
-  title: "Vynaa Valerie | Profile", // Judul halaman
+  title: "wishful | Profile", // Judul halaman
   metaTitle: "Vynaa || Profile", // Meta title untuk SEO
-  metaDescription: "Profile Website Vynaa Valerie, Creator and Dev", // Deskripsi meta untuk SEO
+  metaDescription: "Profile Website wishful, Creator and Dev", // Deskripsi meta untuk SEO
   metaKeywords: "Vynaa, Vynaa Valerie, profile Vynaa, siapa Vynaa?", // Keywords meta untuk SEO
   metaRobots: "index, follow", // Pengaturan robot meta untuk SEO
   imageSrc: "media/vynaa.jpg", // Path ke gambar profil
-  name: "Vynaa Valerie", // Nama profil
+  name: "wishful", // Nama profil
   occupation: "Creator & Developer", // Pekerjaan atau jabatan
   links: [
-    { title: "GitHub", url: "https://github.com/VynaaValerie", icon: "fa-github" }, // Tautan GitHub
-    { title: "Instagram", url: "https://www.instagram.com/vynaa_valerie", icon: "fa-instagram" }, // Tautan Instagram
-    { title: "Telegram", url: "https://t.me/VLShop2", icon: "fa-telegram" }, // Tautan Telegram
-    { title: "Email", url: "mailto:vynaavalerie@example.com", icon: "fa-envelope" }, // Tautan Email
-    { title: "WhatsApp", url: "https://wa.me/6282389924037?text=hai", icon: "fa-whatsapp" }, // Tautan WhatsApp
-    { title: "YouTube", url: "https://youtube.com/@VynaaChan", icon: "fa-youtube" } // Tautan YouTube
+    { title: "Email", url: "mailto:larpfull@gmail.com", icon: "fa-envelope" }, // Tautan Email
   ]
 };
 
