5b8482024f34b8862f36f757fe590e4ef6b44996
wicked1234567 <ly1ngz3n@gmail.com>
2024-10-07 03:46:24 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,17 @@
 // Objek untuk menyimpan semua pengaturan profil
 const profileSettings = {
   favicon: "media/vynaa.jpg", // Path ke favicon
-  title: "Vynaa Valerie | Profile", // Judul halaman
-  metaTitle: "Vynaa || Profile", // Meta title untuk SEO
-  metaDescription: "Profile Website Vynaa Valerie, Creator and Dev", // Deskripsi meta untuk SEO
-  metaKeywords: "Vynaa, Vynaa Valerie, profile Vynaa, siapa Vynaa?", // Keywords meta untuk SEO
+  title: "lostjaix | Profile", // Judul halaman
+  metaTitle: "jai || Profile", // Meta title untuk SEO
+  metaDescription: "Profile Website lostjaix, staff", // Deskripsi meta untuk SEO
+  metaKeywords: "jai, lostjaix, profile jaix, lost?", // Keywords meta untuk SEO
   metaRobots: "index, follow", // Pengaturan robot meta untuk SEO
   imageSrc: "media/vynaa.jpg", // Path ke gambar profil
-  name: "Vynaa Valerie", // Nama profil
-  occupation: "Creator & Developer", // Pekerjaan atau jabatan
+  name: "lostjaix", // Nama profil
+  occupation: "staff and tt editor", // Pekerjaan atau jabatan
   links: [
-    { title: "GitHub", url: "https://github.com/VynaaValerie", icon: "fa-github" }, // Tautan GitHub
-    { title: "Instagram", url: "https://www.instagram.com/vynaa_valerie", icon: "fa-instagram" }, // Tautan Instagram
-    { title: "Telegram", url: "https://t.me/VLShop2", icon: "fa-telegram" }, // Tautan Telegram
-    { title: "Email", url: "mailto:vynaavalerie@example.com", icon: "fa-envelope" }, // Tautan Email
-    { title: "WhatsApp", url: "https://wa.me/6282389924037?text=hai", icon: "fa-whatsapp" }, // Tautan WhatsApp
-    { title: "YouTube", url: "https://youtube.com/@VynaaChan", icon: "fa-youtube" } // Tautan YouTube
-  ]
-};
-
+    { title: "tiktok", url: "https://www.tiktok.com/@jaixtard?_t=8qL74uPxD35&_r=1" // Tautan GitHub
+    
 // Mengatur favicon
 document.getElementById('favicon').href = profileSettings.favicon;
 
